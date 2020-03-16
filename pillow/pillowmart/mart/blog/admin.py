bf30b62506358293edddce9367e5be84de3792c3
NouKD <n09916538@gmail.com>
2020-03-16 18:29:30 +0000

    admin

--- pillow/pillowmart/mart/blog/admin.py
+++ pillow/pillowmart/mart/blog/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 from . import models
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
 
-admin.site.register(models.CategorieArticle)
-admin.site.register(models.Tag)ww
-admin.site.register(models.Article)
-admin.site.register(models.Commentaire)
+
+class CategorieArticleAdmin(admin.ModelAdmin):
+    list_display =  ('nom','date_add', 'date_update', 'status','image_view')
+    list_filter =  ('status',)
+    search_fields = ('nom',)
+    date_hierarchy = 'date_add'
+    list_display_links = ['nom',]
+    ordering = ['nom',]
+    list_per_page = 10
+    fieldsets = [
+        ("infocategory",{'fields':['nom','image','description']}),
+        ("standare",{'fields':['status',]})
+        ]
+
+    def image_view(self,obj):
+        return mark_safe("<img src='{url}'/ width='100px' height='50px'>".format(url=obj.image.url))    
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+_register(models.CategorieArticle, CategorieArticleAdmin)
+
