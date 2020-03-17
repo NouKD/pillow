d934332470e9b3a943679905521a6b25ef530551
NouKD <NouKD-n09916538@gmail.com>
2020-03-17 01:49:19 +0000

    admin

--- pillow/pillowmart/mart/blog/admin.py
+++ pillow/pillowmart/mart/blog/admin.py
@@ -8,3 +8,26 @@ admin.site.register(models.CategorieArticle)
 admin.site.register(models.Tag)
 admin.site.register(models.Article)
 admin.site.register(models.Commentaire)
+
+
+class CategorieArticleAdmin(admin.ModelAdmin):
+    list_display =  ('nom','date_add', 'date_update', 'status','image_view')
+    list_filter =  ('status',)
+    search_fields = ('nom',)
+    date_hierarchy = 'date_add'
+    list_display_links = ['nom',]
+    ordering = ['nom',]
+    list_per_page = 10
+    fieldsets = [
+        ("infocategory",{'fields':['nom','image','description']}),
+        ("standare",{'fields':['status',]})
+        ]
+
+    def image_view(self,obj):
+        return mark_safe("<img src='{url}'/ width='100px' height='50px'>".format(url=obj.image.url))    
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+_register(models.CategorieArticle, CategorieArticleAdmin)
+
